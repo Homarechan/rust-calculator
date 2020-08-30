3eb951fbbe379a4a545d536192859ac465e185a8
Homarechan <tikuwakunn@icloud.com>
2020-08-30 13:26:57 +0900

    Make prompt

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,26 @@ use rust_calculator::calculator;
 use rust_calculator::parser;
 use rust_calculator::tokenizer;
 
+use std::io::{self, Write};
+
 fn main() {
-    let expr: String = "1 + 2 * (3 - 1)\0".to_string();
-    let mut t = tokenizer::Tokenizer::new(expr);
-    t.tokenize().unwrap();
+    loop {
+        let mut input = String::new();
+        print!("> ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut input).unwrap();
 
-    let tokens = t.get_tokens();
-    dbg!(&tokens);
-    let mut parser = parser::Parser::new(tokens);
-    parser.parse().unwrap();
+        let mut t = tokenizer::Tokenizer::new(format!("{}\0", input.trim_end()));
+        t.tokenize().unwrap();
 
-    let tree = parser.get_tree();
-    dbg!(tree);
-    let n = calculator::calculate(tree.clone());
-    println!("{}", n);
+        let tokens = t.get_tokens();
+        dbg!(&tokens);
+        let mut parser = parser::Parser::new(tokens);
+        parser.parse().unwrap();
+
+        let tree = parser.get_tree();
+        dbg!(tree);
+        let n = calculator::calculate(tree.clone());
+        println!("{}", n);
+    }
 }
